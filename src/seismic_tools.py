f68bcae8f2f748862a4a53f1644725787888d6f7
Marco A. Lopez-Sanchez <marcoalopez@protonmail.com>
2023-07-21 14:36:23 +0200

    add license statements

--- src/seismic_tools.py
+++ src/seismic_tools.py
@@ -1,7 +1,32 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/
+# -*- coding: utf-8 -*-
+#######################################################################
+# This file is part of PyRockWave Python module
+#
+# Filename: seismic_tools.py
+# Description: TODO
+#
+# Copyright (c) 2023. 
+#
+# PyRockWave is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyRockWave is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyRockWave.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Marco A. Lopez-Sanchez, http://orcid.org/0000-0002-0261-9267
+# Email: lopezmarco [to be found at] uniovi.es
+# Website: https://marcoalopez.github.io/PyRockWave/
+# Project Repository: https://github.com/marcoalopez/PyRockWave
+#######################################################################
 
+# Import statements
 import numpy as np
 import pandas as pd
 
