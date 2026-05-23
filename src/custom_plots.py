03d4c3748066e79e81bada3dedb762aae096133e
Marco A. Lopez-Sanchez <marcoalopez@protonmail.com>
2026-05-23 19:29:26 +0200

    Create custom_plots.py

---
+++ src/custom_plots.py
@@ -0,0 +1,39 @@
+# =========================================================================== #
+# PyRockWave: A Python Module for modelling elastic properties                #
+# of Earth materials.                                                         #
+#                                                                             #
+# Filename: anisotropic_models.py                                             #
+# Description: This module contains a few custom plots for the PyRockWave     #
+# module.                                                                     #
+#                                                                             #
+# SPDX-License-Identifier: GPL-3.0-or-later                                   #
+# Copyright (c) 2023-present, Marco A. Lopez-Sanchez. All rights reserved.    #
+#                                                                             #
+# PyRockWave is free software: you can redistribute it and/or modify          #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# PyRockWave is distributed in the hope that it will be useful,               #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with PyRockWave. If not, see <http://www.gnu.org/licenses/>.          #
+#                                                                             #
+# Author: Marco A. Lopez-Sanchez                                              #
+# ORCID: http://orcid.org/0000-0002-0261-9267                                 #
+# Email: lopezmarco [to be found at] uniovi dot es                            #
+# Website: https://marcoalopez.github.io/PyRockWave/                          #
+# Repository: https://github.com/marcoalopez/PyRockWave                       #
+# =========================================================================== #
+
+# Import statements
+import matplotlib.pyplot as plt
+
+# Function definitions
+
+
+
+# End of file
